ac692ad982322cb92c9c15d69087386f128038fb
KingWarin <info@kingwarin.de>
2023-12-07 16:27:31 +0100

    Finally solve part2 of day5

--- d5.js
+++ d5.js
@@ -1,50 +1,36 @@
 // Part 1:
 let [seeds,soil,fertilizer,water,light,temperature,humidity,locationMap] = $0.innerText.replace(/\n$/, '').split('\n\n');
-seeds = seeds.replace('seeds: ','').split(' ').map(x => { return {seed: parseInt(x)};});
+seeds = seeds.replace('seeds: ','').split(' ').map(x => { return parseInt(x)});
 let maps = {soil,fertilizer,water,light,temperature,humidity,location:locationMap};
-function getTarget(maps, target, value) {
-  if (!Object.keys(maps[target]).map(x => parseInt(x)).includes(value)) {
-    return value;
-  }
-  return maps[target][value];
-}
+let ranges = {};
 for(let currentMapName of Object.keys(maps)){
     let currentMap = maps[currentMapName];
     currentMap = currentMap.split('\n');
     currentMap.shift();
+    let mapRanges = [];
     currentMap = currentMap.map(x => {
         let [destination, source, length] = x.split(' ');
+        mapRanges.push({start: parseInt(source), finish: parseInt(source) + parseInt(length), offset: parseInt(destination) - parseInt(source)});
         return {source: parseInt(source), destination: parseInt(destination), length: parseInt(length)};
     });
-    let tempMap = {};
-    currentMap.forEach(map => {
-        let target = 0;
-        while(target <= map.length) {
-            let newTarget = target + 100;
-            let breakout = false;
-            if ( newTarget > map.length ) {
-              newTarget = map.length;
-              breakout = true;
-            }
-            for ( let i = target; i < newTarget; i++ ) {
-                tempMap[map.source + i] = map.destination + i;
-            }
-            target = newTarget;
-            console.log(tempMap);
-            if ( breakout ) {
-              target++;
-            }
-        }
-    });
-    maps[currentMapName] = tempMap;
+    maps[currentMapName] = currentMap;
+    ranges[currentMapName] = mapRanges;
 };
 let lowestLocation1 = false;
-seeds = seeds.map(seed => {
-    let currentLocation = getTarget(maps, 'location', getTarget(maps, 'humidity', getTarget(maps, 'temperature', getTarget(maps, 'light', getTarget(maps, 'water', getTarget(maps, 'fertilizer', getTarget(maps, 'soil', seed.seed)))))));
-    if (!lowestLocation1 || currentLocation < lowestLocation1) {
-        lowestLocation1 = currentLocation;
+function getTarget(ranges, target, source) {
+    let map = ranges[target];
+    for(let i = 0; i < map.length; i++) {
+        if ( source >= map[i].start && source <= map[i].finish ) {
+            return source + map[i].offset;
+        }
     }
-    return seed;
+    return source;
+};
+seeds.forEach(seed => {
+    let result = getTarget(ranges, 'location', getTarget(ranges, 'humidity', getTarget(ranges, 'temperature', getTarget(ranges, 'light', getTarget(ranges, 'water', getTarget(ranges, 'fertilizer', getTarget(ranges, 'soil', seed)))))));
+    if ( !lowestLocation1 || result < lowestLocation1 ) {
+        lowestLocation1 = result;
+    }
 });
 console.log(lowestLocation1);
 
@@ -56,9 +42,8 @@ while(seeds.length > 0) {
     let allSeeds = [];
     let start = seeds.shift();
     let length = seeds.shift();
-  console.log(start);
-    for( let i = start; i < start + length; i++ ) {
-        let currentLocation = getTarget(maps, 'location', getTarget(maps, 'humidity', getTarget(maps, 'temperature', getTarget(maps, 'light', getTarget(maps, 'water', getTarget(maps, 'fertilizer', getTarget(maps, 'soil', i)))))));
+        for( let i = start; i < start + length; i++ ) {
+        let currentLocation = getTarget(ranges, 'location', getTarget(ranges, 'humidity', getTarget(ranges, 'temperature', getTarget(ranges, 'light', getTarget(ranges, 'water', getTarget(ranges, 'fertilizer', getTarget(ranges, 'soil', i)))))));
         if (!lowestLocation2 || currentLocation < lowestLocation2) {
             lowestLocation2 = currentLocation;
         }
