f11b62583e34b86288615f8b37334bea036100f5
KingWarin <info@kingwarin.de>
2024-12-01 18:50:01 +0100

    Day1 2024

--- d1.js
+++ d1.js
@@ -1,62 +1,20 @@
-//part 1:
-let lines = $0.textContent.split('\n');
-lines.pop();
-let cleanLines = [];
-lines.forEach(line=>{
-    cleanLines.push(line.replace(/[a-z]/g, ''));
+// Part1
+let s = $0.innerHTML.split('\n').map(x => x.split(/\s/));
+let a1 = [], a2 = [];
+s.forEach(el => {
+  if (el.length > 1) {
+    a1.push(parseInt(el[0]));
+    a2.push(parseInt(el[3]));
+  }
 });
-let values = [];
-cleanLines.forEach(line=>{
-    let value = '';
-    value += line[0];
-    value += line[line.length - 1];
-    values.push(value);
-});
-values.reduce((x,y)=>{return parseInt(x)+parseInt(y);});
+a1 = a1.sort();
+a2 = a2.sort();
+console.log(`Result ${a1.map((el, index) => Math.abs(el - a2[index])).reduce((a,b) => a+b)}`);
 
-//part 2:
-let matcher = function(match, p1, p2, p3, p4, p5, p6, p7, p8, p9, offset, string) {
-    if (p1) {
-        return 'o1e';
-    }
-    if (p2) {
-        return 't2o';
-    }
-    if (p3) {
-        return 't3e';
-    }
-    if (p4) {
-        return 'f4r';
-    }
-    if (p5) {
-        return 'f5e';
-    }
-    if (p6) {
-        return 's6x';
-    }
-    if (p7) {
-        return 's7n';
-    }
-    if (p8) {
-        return 'e8t';
-    }
-    if (p9) {
-        return 'n9e';
-    }
-};
-let lines2 = $0.textContent.split('\n');
-lines2.pop();
-let cleanLines2 = [];
-lines2.forEach(line => {
-    line = line.replace(/(one)|(two)|(three)|(four)|(five)|(six)|(seven)|(eight)|(nine)/g, matcher);
-    line = line.replace(/(one)|(two)|(three)|(four)|(five)|(six)|(seven)|(eight)|(nine)/g, matcher);
-    cleanLines2.push(line.replace(/[a-z]/g, ''));
+// Part2
+let res2 = [];
+a1.map(el => {
+  let count = a2.map(el2 => el2 === el ? 1 : 0).reduce((a,b) => a+b);
+  res2.push(el*count);
 });
-let values2 = [];
-cleanLines2.forEach(line=>{
-    let value = '';
-    value += line[0];
-    value += line[line.length - 1];
-    values2.push(value);
-});
-values2.reduce((x,y)=>{return parseInt(x)+parseInt(y);});
+console.log(`Result ${res2.reduce((a,b)=>a+b)}`);
